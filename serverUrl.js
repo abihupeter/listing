291540915ef7114f5a9683d897661dec499d0a8c
Abihu Peter <abihupita@gmail.com>
2025-06-17 11:30:33 +0300

    images

---
+++ serverUrl.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+let baseURL;
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://127.0.0.1:8000/api";
+  //  baseURL = "https://kodinyumba.app/api";
+
+
+} else {
+  baseURL = "https://kodinyumba.app/api";
+}
+
+export const serverUrl = axios.create({
+  baseURL: baseURL,
+});
