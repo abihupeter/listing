94612fe047d456ca59c43391206efd06bce6b626
Abihu Peter <abihupita@gmail.com>
2025-06-26 10:53:40 +0300

    i

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,14 +3,14 @@
 export default function Navbar({ isScrolled }: { isScrolled: boolean }) {
   return (
     <nav
-      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
+      className={`fixed top-0 left-0 h-13 right-0 z-50 transition-all duration-300 ${
         isScrolled ? "bg-white shadow-md" : "bg-transparent"
       }`}
     >
       <div className="mx-auto px-4 py-4 container">
         <div className="flex justify-center items-center">
           <div
-            className={`text-2xl font-bold transition-colors duration-300 ${
+            className={`font-[fonarto] text-4xl h-9 font-bold transition-colors duration-300 ${
               isScrolled ? "text-blue-600" : "text-blue-500"
             }`}
           >
