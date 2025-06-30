6c21b8c9482756fb3f3a660e83ca9d762c2af8c8
Abihu Peter <abihupita@gmail.com>
2025-06-30 10:44:47 +0300

    i

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
         <div className="gap-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5">
           {/* Kodi Logo */}
           <div className="lg:col-span-1">
-            <div className="font-[fonarto] text-white text-[40px] font-bold text-3xl">Kodi</div>
+            <div className="font-[fonarto] text-white text-[50px] font-bold text-4xl">Kodi</div>
           </div>
 
           {/* Head Offices - Nairobi */}
@@ -25,7 +25,9 @@ export function Footer() {
         <div className="mt-12 pt-8 border-slate-700 border-t">
           <div className="flex md:flex-row flex-col justify-between items-center gap-4">
             <div className="text-white text-sm"></div>
-            <div className="text-white text-sm">KSH ©2025 powered by Kodi. All rights reserved.</div>
+            <div className="text-white text-sm">
+              <span className="underline">KSH</span>    ©2025 powered by Kodi. All rights reserved.
+            </div>
           </div>
         </div>
       </div>
